ac916070c34d3225a610d390ecb858041612716d
zacode123 <zuliabegum0@gmail.com>
2026-06-08 21:16:02 +0530

    Update interface.cpp

--- boards/marauder-mini/interface.cpp
+++ boards/marauder-mini/interface.cpp
@@ -14,7 +14,7 @@ void _setup_gpio() {
     pinMode(L_BTN, INPUT_PULLUP);
 
     bruceConfig.colorInverted = 0;
-    bruceConfigPins.rotation = 0; // portrait mode for Phantom
+    bruceConfigPins.rotation = 1;
 }
 
 /***************************************************************************************
