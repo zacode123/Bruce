339fc07248c8a531c167434cc5bf1ef38a201177
zacode123 <zuliabegum0@gmail.com>
2026-06-09 20:40:40 +0530

    Update pins_arduino.h

--- boards/marauder-mini/pins_arduino.h
+++ boards/marauder-mini/pins_arduino.h
@@ -78,12 +78,6 @@ static const uint8_t ADC2 = 8;
 #define CC1101_SCK_PIN SPI_SCK_PIN
 #define CC1101_MISO_PIN SPI_MISO_PIN
 
-#define NRF24_CE_PIN 21
-#define NRF24_SS_PIN SPI_SS_PIN
-#define NRF24_MOSI_PIN SPI_MOSI_PIN
-#define NRF24_SCK_PIN SPI_SCK_PIN
-#define NRF24_MISO_PIN SPI_MISO_PIN
-
 #define FP 1
 #define FM 1
 #define FG 2
